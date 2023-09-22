cf78b0ed6000e5dabf6bfd4e75f0ed637b7b27ed
Artyom D <d.tem4ik@gmail.com>
2023-09-22 12:14:44 +0600

    remove primary-darken-1

--- src/runtime/plugins/vuetify.ts
+++ src/runtime/plugins/vuetify.ts
@@ -19,7 +19,6 @@ export default defineNuxtPlugin((nuxtApp) => {
       background: '#FFFFFF',
       surface: '#FFFFFF',
       primary: '#4D4E52',
-      'primary-darken-1': '#3700B3',
       secondary: '#03DAC6',
       'secondary-darken-1': '#018786',
       error: '#B00020',
