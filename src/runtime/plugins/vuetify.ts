d46d047dda9ffe1c38fc9ebe2d99f66564c36bdc
Artyom D <d.tem4ik@gmail.com>
2023-09-22 12:10:20 +0600

    theme example

--- src/runtime/plugins/vuetify.ts
+++ src/runtime/plugins/vuetify.ts
@@ -13,6 +13,22 @@ const aliases = allAliases;
 
 export default defineNuxtPlugin((nuxtApp) => {
 
+  const myCustomLightTheme = {
+    dark: false,
+    colors: {
+      background: '#FFFFFF',
+      surface: '#FFFFFF',
+      primary: '#4D4E52',
+      'primary-darken-1': '#3700B3',
+      secondary: '#03DAC6',
+      'secondary-darken-1': '#018786',
+      error: '#B00020',
+      info: '#2196F3',
+      success: '#4CAF50',
+      warning: '#FB8C00',
+    },
+  }
+
   const vuetify = createVuetify({
     components,
     directives,
@@ -20,6 +36,12 @@ export default defineNuxtPlugin((nuxtApp) => {
       defaultSet: 'mdi',
       aliases,
       sets: { mdi }
+    },
+    theme: {
+      defaultTheme: 'myCustomLightTheme',
+      themes: {
+        myCustomLightTheme
+      }
     }
   });
 
