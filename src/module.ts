118c69cbc529f95ae935792038b578384f33a751
AibtVlr <valeryaibatov@gmail.com>
2023-10-06 17:57:03 +0600

    Update module.ts

--- src/module.ts
+++ src/module.ts
@@ -35,11 +35,26 @@ export default defineNuxtModule<ModuleOptions>({
       name: 'partsCard', // name of the component to be used in vue templates
       filePath: resolver.resolve('runtime/components/Parts/Card.vue')
     })
-    
+
     // Headers
     addComponent({
       name: 'HeaderOne',
       filePath: resolver.resolve('runtime/components/Parts/HeaderOne.vue')
+    })
+
+    addComponent({
+      name: 'HeaderTwo',
+      filePath: resolver.resolve('runtime/components/Parts/HeaderTwo.vue')
+    })
+
+    addComponent({
+      name: 'HeaderThree',
+      filePath: resolver.resolve('runtime/components/Parts/HeaderThree.vue')
+    })
+
+    addComponent({
+      name: 'HeaderFour',
+      filePath: resolver.resolve('runtime/components/Parts/HeaderFour.vue')
     })
   }
 })
