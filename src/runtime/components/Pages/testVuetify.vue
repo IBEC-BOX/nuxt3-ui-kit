76e401f81e3df241b7affa5fa754843cf7353b72
AibtVlr <valeryaibatov@gmail.com>
2023-10-10 17:21:16 +0600

    update testVuetify

--- src/runtime/components/Pages/testVuetify.vue
+++ src/runtime/components/Pages/testVuetify.vue
@@ -1,151 +1,26 @@
 <template>
   <v-app>
-    <v-navigation-drawer temporary v-model="showDrawer" location="right">
-      <div class="w-100 text-right">
-        <v-btn flat icon="$close" size="x-large" @click="toggle" />
-      </div>
-      <p class="text-center"><b>I'm the drawer</b></p>
-    </v-navigation-drawer>
-
     <!-- Header first variant -->
-    <HeaderOne
-      header-active-link-variant="bottom_line"
-      top-background="grey-lighten-2"
-      :menu="header_menu"
+    <partsHeader
+      header-active-link-variant="bottom_line black"
+      variant="tabs"
       :buttons="header_buttons"
-    />
+      :menu="header_menu"
+      :top-menu="header_top_menu_links"
+      :top-links="header_top_links"
+      :tabs="header_tabs"
+    >
+      <template #append-top>
+        append top slot
+      </template>
+
+      <template #buttons>
+        buttons slot
+      </template>
+    </partsHeader>
     <!-- Header first variant END -->
 
-    <!-- Header second variant -->
-    <HeaderTwo
-      header-active-link-variant="bottom_line"
-      top-background="grey-lighten-3"
-      top-class="border-bottom"
-      :top-menu="header_top_menu"
-      :menu="header_menu"
-      :top-links="header_top_links"
-      :buttons="header_buttons_second"
-    >
-      <template #append-top>
-        <v-menu>
-          <template #activator="{ props }">
-            <v-btn v-bind="props">
-              <v-img width="20" height="20" class="rounded-circle mr-1" cover :src="selected_lang.image" />
-              <span class="text-body-2 text-grey-darken-2">
-                {{ selected_lang.title }}
-              </span>
-            </v-btn>
-          </template>
-
-          <v-list>
-            <v-list-item
-              v-for="(item, i) in langs"
-              :key="i"
-            >
-              <v-list-item-title class="cursor-pointer p-0">
-                <v-btn variant="text" @click="selected_lang = item">
-                  <v-img width="20" height="20" class="rounded-circle mr-1" cover :src="item.image" />
-                  <span class="text-body-2 text-grey-darken-3">
-                    {{ item.title }}
-                  </span>
-                </v-btn>
-              </v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-
-        <v-btn icon="mdi-magnify" size="small" class="text-grey-darken-1" />
-      </template>
-    </HeaderTwo>
-    <!-- Header second variant END -->
-
-    <!-- Header third variant -->
-    <HeaderThree
-      :tabs="header_tabs"
-      :buttons="header_buttons"
-      :top-links="header_top_links"
-    >
-      <template #append-top>
-        <v-menu>
-          <template #activator="{ props }">
-            <v-btn v-bind="props">
-              <v-img width="20" height="20" class="rounded-circle mr-1" cover :src="selected_lang.image" />
-              <span class="text-body-2 text-grey-darken-2">
-                {{ selected_lang.title }}
-              </span>
-            </v-btn>
-          </template>
-
-          <v-list>
-            <v-list-item
-              v-for="(item, i) in langs"
-              :key="i"
-            >
-              <v-list-item-title class="cursor-pointer p-0">
-                <v-btn variant="text" @click="selected_lang = item">
-                  <v-img width="20" height="20" class="rounded-circle mr-1" cover :src="item.image" />
-                  <span class="text-body-2 text-grey-darken-3">
-                    {{ item.title }}
-                  </span>
-                </v-btn>
-              </v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </template>
-    </HeaderThree>
-    <!-- Header third variant END -->
-
-    <!-- Header fourth variant -->
-    <HeaderFour
-      header-active-link-variant="bottom_line"
-      header-height="88"
-      top-background="grey-lighten-2"
-      :menu="header_menu"
-      :menu-top-links="header_top_menu_links"
-    >
-      <template #buttons>
-        <v-menu>
-          <template #activator="{ props }">
-            <v-btn v-bind="props">
-              <v-img width="20" height="20" class="rounded-circle mr-1" cover :src="selected_lang.image" />
-              <span class="text-body-2 text-grey-darken-2">
-                {{ selected_lang.title }}
-              </span>
-            </v-btn>
-          </template>
-
-          <v-list>
-            <v-list-item
-              v-for="(item, i) in langs"
-              :key="i"
-            >
-              <v-list-item-title class="cursor-pointer p-0">
-                <v-btn variant="text" @click="selected_lang = item">
-                  <v-img width="20" height="20" class="rounded-circle mr-1" cover :src="item.image" />
-                  <span class="text-body-2 text-grey-darken-3">
-                    {{ item.title }}
-                  </span>
-                </v-btn>
-              </v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-
-        <v-btn variant="icon" icon="mdi-magnify" class="text-grey-darken-1" />
-      </template>
-    </HeaderFour>
-    <!-- Header fourth variant END -->
-
-    <v-main class="d-flex align-center">
-      <v-container>
-        <v-sheet elevation="5" class="py-4 text-center">
-          <h1 class="text-18">Built and styled with Vuetify 3</h1>
-          <p>Minimized production bundle (70kB in tot)</p>
-          <v-btn class="mt-4">press me to ripple</v-btn>
-        </v-sheet>
-      </v-container>
-    </v-main>
+    <NuxtPage />
 
     <v-footer app elevation="5">
       <v-row no-gutters justify="center"
@@ -163,193 +38,80 @@
 
 <script setup>
 import { ref } from 'vue'
-import Card from "../Parts/Card.vue"
-import footerOne from "../Parts/footer/footer.vue";
-import HeaderOne from '../Parts/header/HeaderOne.vue'
-import HeaderTwo from '../Parts/header/HeaderTwo.vue'
-import HeaderThree from '../Parts/header/HeaderThree.vue'
-import HeaderFour from '../Parts/header/HeaderFour.vue'
+import partsHeader from '../Parts/header/header.vue'
 
 const showDrawer = ref(false);
 
-const statusVacancy = {
-  text: "Reply without resume",
-  status: "success"
-}
-
-const firstStyleTop = {
-  "text_top": {
-    title: "Let’s get started on something great",
-    subtitle: "Join over 4,000+ startups already growing with Untitled."
-  },
-  "button_top": [
-    {
-      id: 1,
-      backgroundColor: "",
-      rounded: "lg",
-      leftIcon: "$vuetify",
-      text: "Medium Button",
-      size: ""
-    },
-    {
-      id: 2,
-      backgroundColor: "primary",
-      rounded: "lg",
-      text: "Medium Button",
-      size: ""
-    }
-  ]
-}
-
-const secondStyleTop = {
-  title: "Join our newsletter",
-  subtitle: "We’ll send you a nice letter once per week. No spam."
-}
-
-const menu = [
+const testButtonNews = () => console.log(cards[1].id)
+const cards = [
   {
     id: 1,
-    title: "Product",
-    links: [
-      {
-        id: 1,
-        text: "Overview",
-        link: "/",
-      },
-      {
-        id: 2,
-        text: "Features",
-        link: "",
-      },
-      {
-        id: 3,
-        text: "Solutions",
-        link: "",
-      },
-      {
-        id: 4,
-        text: "Tutorials",
-        link: "",
-      },
-      {
-        id: 5,
-        text: "Pricing",
-        link: "",
-      },
-      {
-        id: 6,
-        text: "Releases",
-        link: "",
-      },
-    ]
+    dateAuthorRight: false,
+    horizontalCard: false,
+    width: 349,
+    minHeight: 414,
+    variantCard: "elevated",
+    styleCard: "vacancy",
+    imgSrc: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
+    imgAlt: "Картинка",
+    title: "Card Title",
+    text: "A “card” is a UI design pattern that groups related information in a flexible-size container visually resembling a playing card.",
+    date: "02.04.2023",
+    author: "by Tyler, The Creator",
+    functionButton: "",
+    textButton: "Читать",
+    iconButton: "",
+    prependIconButton: "$vuetify",
+    appendIconButton: "",
   },
   {
     id: 2,
-    title: "Product",
-    links: [
-      {
-        id: 1,
-        text: "Overview",
-        link: "",
-      },
-      {
-        id: 2,
-        text: "Features",
-        link: "",
-      },
-      {
-        id: 3,
-        text: "Solutions",
-        link: "",
-      },
-      {
-        id: 4,
-        text: "Tutorials",
-        link: "",
-      },
-      {
-        id: 5,
-        text: "Pricing",
-        link: "",
-      },
-      {
-        id: 6,
-        text: "Releases",
-        link: "",
-      },
-    ]
+    dateAuthorRight: false,
+    horizontalCard: false,
+    width: 349,
+    variantCard: "elevated",
+    styleCard: "standard",
+    imgSrc: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
+    imgAlt: "Картинка",
+    title: "Card Title",
+    text: "A “card” is a UI design pattern that groups related information in a flexible-size container visually resembling a playing card.",
+    date: "02.04.2023",
+    author: "by Tyler, The Creator",
+    functionButton: testButtonNews,
+    textButton: "Читать",
   },
   {
     id: 3,
-    title: "Product",
-    links: [
-      {
-        id: 1,
-        text: "Overview",
-        link: "",
-      },
-      {
-        id: 2,
-        text: "Features",
-        link: "",
-      },
-      {
-        id: 3,
-        text: "Solutions",
-        link: "",
-      },
-      {
-        id: 4,
-        text: "Tutorials",
-        link: "",
-      },
-      {
-        id: 5,
-        text: "Pricing",
-        link: "",
-      },
-      {
-        id: 6,
-        text: "Releases",
-        link: "",
-      },
-    ]
+    dateAuthorRight: false,
+    horizontalCard: false,
+    width: 349,
+    variantCard: "elevated",
+    styleCard: "standard",
+    imgSrc: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
+    imgAlt: "Картинка",
+    title: "Card Title",
+    text: "A “card” is a UI design pattern that groups related information in a flexible-size container visually resembling a playing card.",
+    date: "02.04.2023",
+    author: "by Tyler, The Creator",
+    functionButton: "",
+    textButton: "Читать",
   },
   {
     id: 4,
-    title: "Product",
-    links: [
-      {
-        id: 1,
-        text: "Overview",
-        link: "",
-      },
-      {
-        id: 2,
-        text: "Features",
-        link: "",
-      },
-      {
-        id: 3,
-        text: "Solutions",
-        link: "",
-      },
-      {
-        id: 4,
-        text: "Tutorials",
-        link: "",
-      },
-      {
-        id: 5,
-        text: "Pricing",
-        link: "",
-      },
-      {
-        id: 6,
-        text: "Releases",
-        link: "",
-      },
-    ]
+    dateAuthorRight: false,
+    horizontalCard: false,
+    width: 349,
+    minHeight: 414,
+    variantCard: "elevated",
+    styleCard: "standard",
+    imgSrc: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
+    imgAlt: "Картинка",
+    title: "Card Title",
+    text: "A “card” is a UI design pattern that groups related information in a flexible-size container visually resembling a playing card.",
+    date: "02.04.2023",
+    author: "by Tyler, The Creator",
+    functionButton: "",
+    textButton: "Читать",
   },
   {
     id: 5,
@@ -427,49 +189,60 @@ const nav = [
   },
   {
     id: 5,
-    text: "Help centre",
     link: "/",
+    dateAuthorRight: false,
+    horizontalCard: false,
+    width: 349,
+    minHeight: 414,
+    variantCard: "elevated",
+    styleCard: "standard",
+    imgSrc: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
+    imgAlt: "Картинка",
+    title: "Card Title",
+    text: "A “card” is a UI design pattern that groups related information in a flexible-size container visually resembling a playing card.",
+    date: "02.04.2023",
+    author: "by Tyler, The Creator",
+    functionButton: "",
+    textButton: "Читать",
   },
   {
     id: 6,
-    text: "Privacy",
-    link: "/",
-  },
+    dateAuthorRight: false,
+    horizontalCard: false,
+    width: 349,
+    minHeight: 414,
+    variantCard: "elevated",
+    styleCard: "standard",
+    imgSrc: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
+    imgAlt: "Картинка",
+    title: "Card Title",
+    text: "A “card” is a UI design pattern that groups related information in a flexible-size container visually resembling a playing card.",
+    date: "02.04.2023",
+    author: "by Tyler, The Creator",
+    functionButton: "",
+    textButton: "Читать",
+    appendIconButton: "$vuetify",
+  }
 ]
-
-const copyright = "2023 Untitled UI. All rights reserverd"
-const social = [
-  {
-    id: 1,
-    img: "https://cdn.vuetifyjs.com/images/parallax/material.jpg",
-    link: ""
-  },
-  {
-    id: 2,
-    img: "",
-    link: ""
-  },
-  {
-    id: 3,
-    img: "",
-    link: ""
-  },
-  {
-    id: 4,
-    img: "",
-    link: ""
-  },
-  {
-    id: 5,
-    img: "",
-    link: ""
-  },
-  {
-    id: 6,
-    img: "",
-    link: ""
-  },
-]
+const settingsCol = {
+  cols: "",
+  sm: "",
+  md: "6",
+  lg: "",
+  xl: "",
+  xxl: "",
+}
+const settingsButton = {
+  variant: "text",
+  stacked: false,
+  density: "compact",
+  size: "",
+  block: false,
+  rounded: "",
+  elevation: 0,
+  ripple: false,
+  color: ""
+}
 
 const toggle= () => {
   console.log('usedToggle()');
@@ -480,37 +253,37 @@ const header_menu = [
   {
     id: 0,
     title: 'Home',
-    link: '/',
+    url: '/',
   },
 
   {
     id: 0,
     title: 'About us',
-    link: '#',
+    url: '/about',
   },
 
   {
     id: 0,
     title: 'Services',
-    link: '#',
+    url: '',
   },
 
   {
     id: 0,
     title: 'Projects',
-    link: '#',
+    url: '',
   },
 
   {
     id: 0,
     title: 'Team',
-    link: '#',
+    url: '',
   },
 
   {
     id: 0,
     title: 'Career',
-    link: '#',
+    url: '',
   },
 ]
 
@@ -609,296 +382,43 @@ const header_tabs = [
   {
     id: 0,
     title: 'Bank',
-    list: [
-      {
-        id: '0',
-        title: 'Credit cards',
-        link: '#'
-      },
-
-      {
-        id: '2',
-        title: 'Debit cards',
-        link: '#'
-      },
-
-      {
-        id: '3',
-        title: 'Services',
-        link: '#'
-      },
-
-      {
-        id: '4',
-        title: 'Projects',
-        link: '#'
-      },
-
-      {
-        id: '5',
-        title: 'Team',
-        link: '#'
-      },
-
-      {
-        id: '1',
-        title: 'Career',
-        link: '#'
-      },
-    ]
+    url: '/',
   },
 
   {
     id: 1,
     title: 'Business',
-    list: [
-      {
-        id: '0',
-        title: 'Credit cards',
-        link: '#'
-      },
-
-      {
-        id: '2',
-        title: 'Debit cards',
-        link: '#'
-      },
-
-      {
-        id: '3',
-        title: 'Services',
-        link: '#'
-      },
-
-      {
-        id: '4',
-        title: 'Projects',
-        link: '#'
-      },
-
-      {
-        id: '5',
-        title: 'Team',
-        link: '#'
-      },
-
-      {
-        id: '1',
-        title: 'Career',
-        link: '#'
-      },
-    ]
+    url: 'about',
   },
 
   {
     id: 2,
     title: 'Checkout',
-    list: [
-      {
-        id: '0',
-        title: 'Credit cards',
-        link: '#'
-      },
-
-      {
-        id: '2',
-        title: 'Debit cards',
-        link: '#'
-      },
-
-      {
-        id: '3',
-        title: 'Services',
-        link: '#'
-      },
-
-      {
-        id: '4',
-        title: 'Projects',
-        link: '#'
-      },
-
-      {
-        id: '5',
-        title: 'Team',
-        link: '#'
-      },
-
-      {
-        id: '1',
-        title: 'Career',
-        link: '#'
-      },
-    ]
+    url: '',
   },
 
   {
     id: 3,
     title: 'Investment',
-    list: [
-      {
-        id: '0',
-        title: 'Credit cards',
-        link: '#'
-      },
-
-      {
-        id: '2',
-        title: 'Debit cards',
-        link: '#'
-      },
-
-      {
-        id: '3',
-        title: 'Services',
-        link: '#'
-      },
-
-      {
-        id: '4',
-        title: 'Projects',
-        link: '#'
-      },
-
-      {
-        id: '5',
-        title: 'Team',
-        link: '#'
-      },
-
-      {
-        id: '1',
-        title: 'Career',
-        link: '#'
-      },
-    ]
+    url: '',
   },
-
   {
     id: 4,
     title: 'Insurance',
-    list: [
-      {
-        id: '0',
-        title: 'Credit cards',
-        link: '#'
-      },
-
-      {
-        id: '2',
-        title: 'Debit cards',
-        link: '#'
-      },
-
-      {
-        id: '3',
-        title: 'Services',
-        link: '#'
-      },
-
-      {
-        id: '4',
-        title: 'Projects',
-        link: '#'
-      },
-
-      {
-        id: '5',
-        title: 'Team',
-        link: '#'
-      },
-
-      {
-        id: '1',
-        title: 'Career',
-        link: '#'
-      },
-    ]
+    url: '',
   },
 
   {
     id: 5,
     title: 'Travel',
-    list: [
-      {
-        id: '0',
-        title: 'Credit cards',
-        link: '#'
-      },
-
-      {
-        id: '2',
-        title: 'Debit cards',
-        link: '#'
-      },
-
-      {
-        id: '3',
-        title: 'Services',
-        link: '#'
-      },
-
-      {
-        id: '4',
-        title: 'Projects',
-        link: '#'
-      },
-
-      {
-        id: '5',
-        title: 'Team',
-        link: '#'
-      },
-
-      {
-        id: '1',
-        title: 'Career',
-        link: '#'
-      },
-    ]
+    url: '',
   },
 
   {
     id: 6,
     title: 'City',
-    list: [
-      {
-        id: '0',
-        title: 'Credit cards',
-        link: '#'
-      },
-
-      {
-        id: '2',
-        title: 'Debit cards',
-        link: '#'
-      },
-
-      {
-        id: '3',
-        title: 'Services',
-        link: '#'
-      },
-
-      {
-        id: '4',
-        title: 'Projects',
-        link: '#'
-      },
-
-      {
-        id: '5',
-        title: 'Team',
-        link: '#'
-      },
-
-      {
-        id: '1',
-        title: 'Career',
-        link: '#'
-      },
-    ]
-  },
+    url: '',
+  }
 ]
 
 const selected_lang = ref({
