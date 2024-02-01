44b3a408a94bb69623300f8a63bd955e1f99abb3
Aidosgd <aidosgd@gmail.com>
2024-02-01 16:57:44 +0600

    fix: stories

--- src/stories/Header.stories.js
+++ src/stories/Header.stories.js
@@ -1,4 +1,4 @@
-import Header from '../runtime/components/Parts/Header/default.vue';
+import Header from '../runtime/components/Parts/Header/Default.vue';
 
 // More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
 export default {
