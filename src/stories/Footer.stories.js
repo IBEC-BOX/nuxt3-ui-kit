f38aee3a6ef9dc56c72cd12811a0426e74cef615
Aidosgd <aidosgd@gmail.com>
2024-02-01 16:59:33 +0600

    fix: stories

--- src/stories/Footer.stories.js
+++ src/stories/Footer.stories.js
@@ -1,4 +1,4 @@
-import Footer from '../runtime/components/Parts/Footer/default.vue';
+import Footer from '../runtime/components/Parts/Footer/Default.vue';
 
 // More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
 export default {
