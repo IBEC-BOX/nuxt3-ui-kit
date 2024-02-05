9088e5f59187a0b405a77ac6402b304bdb09ab36
Евгений Головкин <mr.happy.2000@mail.ru>
2024-02-05 12:41:29 +0600

    update footer

--- src/runtime/components/Pages/TestVuetify.vue
+++ src/runtime/components/Pages/TestVuetify.vue
@@ -4,6 +4,15 @@
     </partsHeaderErg>
 
     <NuxtPage />
+
+    <partsFooterDefault
+      :logo-in-body="true"
+      :logo="{title: 'АО', colorClass: 'text-white'}"
+      :nav="[{id:1,text:'Overview',link:'/'},{id:2,text:'Features',link:'/'},{id:3,text:'Pricing',link:'/'},{id:4,text:'Careers',link:'/'},{id:5,text:'Help centre',link:'/'}, {id:6,text:'Privacy',link:'/'}]"
+      copyright="2023 Untitled UI. All rights reserverd"
+    >
+
+    </partsFooterDefault>
   </v-app>
 </template>
 
