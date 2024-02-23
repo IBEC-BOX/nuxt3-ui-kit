ddcd130984593689fce99603393af1833b892dd1
Евгений Головкин <mr.happy.2000@mail.ru>
2024-02-23 16:09:54 +0600

    edits

--- src/runtime/components/Pages/TestVuetify.vue
+++ src/runtime/components/Pages/TestVuetify.vue
@@ -5,67 +5,68 @@
     />
     <NuxtPage />
     <section class="h-100">
-      <v-container class="bg-amber px-4">
+      <v-container class=" px-4">
 
-        <partsSlider
-          :slider-loop="true"
-          :slider-container="true"
-          :slider-centered-slides="false"
-          :control-button-in-slides="true"
-          :slides="slidesSocial"
-          :height="400"
-          control-buttons-size="small"
-          control-buttons-color="white"
-          control-buttons-rounded="lg"
-          control-buttons-class="mr-2"
-          control-buttons-elevation="0"
-          control-buttons-align="left-bottom"
-          :slider-pagination="true"
-          :slider-pagination-type="'dot'"
-          :sliderPaginationCenter="true"
+        <partsForms
+          title="Ответим на все ваши вопросы"
+          :style-form="'onlyForm'"
+          :inputs="[
+                  {
+                    rules: [
+                      u => !!u || 'Имя обязательно к заполнению',
+                      u => !u || u.length <= 50 || 'Имя не должно быть длиннее 50 символов'
+                    ],
+                    colsAttrs: {
+                      cols: 12
+                    },
+                    attrs: {
+                      variant: 'solo',
+                      placeholder: 'Укажите ваше имя',
+                      flat: true,
+                      rounded: '16',
+                      clearable: true
+                    },
+                    value: '',
+                  },
+                  {
+                    placeholder: '+7',
+                    colsAttrs: {
+                      cols: 12
+                    },
+                    rules: [
+                      u => !!u || 'Номер обязателен к заполнению',
+                      u => !u || u.length === 18 || 'Такого номера не существует'
+                    ],
+                    maska: {mask: '+7 (###) ### ## ##'},
+                    value: '+7',
+                    attrs: {
+                      variant: 'solo',
+                      placeholder: '+7',
+                      flat: true,
+                      rounded: '16',
+                      clearable: true
+                    },
+                  }
+                ]"
+          :button-setting="{
+                  text: 'Отправить',
+                  attrs: {
+                    block: true,
+                    size: 'x-large',
+                    rounded: 'xl',
+                    color: 'dark-1',
+                    class: 'text-none mb-10',
+                  }
+                }"
+          :checkbox-setting="{
+            attrs: {
+                          class: 'custom-checkbox',
+
+            },
+                  label: '<span>Даю свое согласие на сбор и обработку данных в соответствии с <a href=`/`>Политикой конфедициальности</a></span>'
+                }"
         />
 
-
-<!--        <partsErgVacancies-->
-<!--          sheet-rounded="xl"-->
-<!--          sheet-class="py-0 pt-10 py-md-12 px-4 px-md-8 h-100"-->
-<!--          sheet-max-height="450px"-->
-<!--          title="Станьте частью команды"-->
-<!--          sub-title="Мы предлагаем конкурентные зарплаты, социальные гарантии"-->
-<!--          title-class="text-40 text-white font-weight-semi-bold"-->
-<!--          sub-title-class="text-h5 text-white"-->
-<!--          sub-title-style="max-width: 521px"-->
-<!--          :button="{-->
-<!--            to: '/',-->
-<!--            text: 'Посмотреть вакансии',-->
-<!--            image: '/svgIcon/arrow-right.svg',-->
-<!--            attrs: {-->
-<!--              size: 'large',-->
-<!--              rounded: 'xl',-->
-<!--              class: 'text-body-1 font-weight-regular mt-4 py-1',-->
-<!--            }-->
-<!--          }"-->
-<!--          :gallery="true"-->
-<!--          :gallery-hover-effect="true"-->
-<!--          :gallery-images="{0:{chip:{attrs:{variant:'flat',color:'green'},text:'asd'},src:'https://s3-alpha-sig.figma.com/img/e589/51dd/87467ea8ed3307f2aa67a4fff16d45cb?Expires=1708300800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XPOgN9cWiL5lNQKNQpEWyYKNUfdcufsLMvRSBXfg5tryvP2T9GyIh-BycwK2gfla1o1G3ufvC6KxW64biT8r~GV63AFYFIzdkohh7Ya5wYLKv3eHBBzqAZdrV0PtKWJil3HFkSa0TuwRmriW~rf1nuPJx-W~qkkVu1A~sZTvid6G9sIblyijcKz2YRHoDiWxLc4iKJyyK1~FTzj75P3z8szwQzH08A6LG97a5lawt2uAH1f1R-3htFf7xEi5W86IWC4s4byN94KFewoJ9Qki-174MHHVq24BgUkZ7cLPHNvQthXJ5brtbR~4xM83ns~hEjcIJpHdCDGj2~YuTvcrmQ__',text:'absdffdsfdsfsdfdsds'},1:{chip:{attrs:{variant:'flat',color:'green'},text:'asd'},src:'https://s3-alpha-sig.figma.com/img/e589/51dd/87467ea8ed3307f2aa67a4fff16d45cb?Expires=1708300800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XPOgN9cWiL5lNQKNQpEWyYKNUfdcufsLMvRSBXfg5tryvP2T9GyIh-BycwK2gfla1o1G3ufvC6KxW64biT8r~GV63AFYFIzdkohh7Ya5wYLKv3eHBBzqAZdrV0PtKWJil3HFkSa0TuwRmriW~rf1nuPJx-W~qkkVu1A~sZTvid6G9sIblyijcKz2YRHoDiWxLc4iKJyyK1~FTzj75P3z8szwQzH08A6LG97a5lawt2uAH1f1R-3htFf7xEi5W86IWC4s4byN94KFewoJ9Qki-174MHHVq24BgUkZ7cLPHNvQthXJ5brtbR~4xM83ns~hEjcIJpHdCDGj2~YuTvcrmQ__',text:'absdffdsfdsfsdfdsds'},2:{chip:{attrs:{variant:'flat',color:'green'},text:'asd'},src:'https://s3-alpha-sig.figma.com/img/e589/51dd/87467ea8ed3307f2aa67a4fff16d45cb?Expires=1708300800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XPOgN9cWiL5lNQKNQpEWyYKNUfdcufsLMvRSBXfg5tryvP2T9GyIh-BycwK2gfla1o1G3ufvC6KxW64biT8r~GV63AFYFIzdkohh7Ya5wYLKv3eHBBzqAZdrV0PtKWJil3HFkSa0TuwRmriW~rf1nuPJx-W~qkkVu1A~sZTvid6G9sIblyijcKz2YRHoDiWxLc4iKJyyK1~FTzj75P3z8szwQzH08A6LG97a5lawt2uAH1f1R-3htFf7xEi5W86IWC4s4byN94KFewoJ9Qki-174MHHVq24BgUkZ7cLPHNvQthXJ5brtbR~4xM83ns~hEjcIJpHdCDGj2~YuTvcrmQ__',text:'absdffdsfdsfsdfdsds'},3:{chip:{attrs:{variant:'flat',color:'green'},text:'asd'},src:'https://s3-alpha-sig.figma.com/img/e589/51dd/87467ea8ed3307f2aa67a4fff16d45cb?Expires=1708300800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XPOgN9cWiL5lNQKNQpEWyYKNUfdcufsLMvRSBXfg5tryvP2T9GyIh-BycwK2gfla1o1G3ufvC6KxW64biT8r~GV63AFYFIzdkohh7Ya5wYLKv3eHBBzqAZdrV0PtKWJil3HFkSa0TuwRmriW~rf1nuPJx-W~qkkVu1A~sZTvid6G9sIblyijcKz2YRHoDiWxLc4iKJyyK1~FTzj75P3z8szwQzH08A6LG97a5lawt2uAH1f1R-3htFf7xEi5W86IWC4s4byN94KFewoJ9Qki-174MHHVq24BgUkZ7cLPHNvQthXJ5brtbR~4xM83ns~hEjcIJpHdCDGj2~YuTvcrmQ__',text:'absdffdsfdsfsdfdsds'}}"-->
-<!--          :backgroundImage="{-->
-<!--            image: 'https://s3-alpha-sig.figma.com/img/e589/51dd/87467ea8ed3307f2aa67a4fff16d45cb?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=g7EhfQACZT85bZGANQJmhBUgX7KHl9MVIHs8uvYxovf1~mMGheRUpQe4etAQ6EnrUd77Pj9k~JyXNKHx9G9GtdGM8~H6EDQeBXcXz9xZ1PhDjDfXQAUijJ1JN4XNnMWuXIHCqBPPekxCexn3huHy~G3d-WjDd63cPcZek-GtH6QEgck6bYG-HyNnx6QBKVdvynqZS8WMDjTQwMpKUCvYTb59s4eLt-aD4-fc2cZ1qcOqYg4bzzoiJah2DkFTTFI12q-vWdWuwYTmTk2U9QiAAQ0NQOnE3agSmB0ABr2DjObMiMr4SBuDqWKqEXOZT3AC8yYhtHBTC-oH0P9dAxIFxw__',-->
-<!--            attrs: {-->
-<!--              class: 'mb-6',-->
-<!--              style: 'border-radius: 32px',-->
-<!--              'max-height': '712px',-->
-<!--              cover: true,-->
-<!--            }-->
-<!--          }"-->
-<!--          :chip="{-->
-<!--            text: 'Вакансии',-->
-<!--            attrs: {-->
-<!--              color: 'white',-->
-<!--              class: 'text-uppercase mb-8 mt-4',-->
-<!--              variant: 'outlined'-->
-<!--            }-->
-<!--          }"-->
-<!--        />-->
 
       </v-container>
     </section>
@@ -91,72 +92,6 @@
 </template>
 
 <script setup>
-
-const slidesSocial = [
-  {
-    id: 0,
-    chip: {
-      text: 'Социальные проекты',
-      attrs: {
-        color: 'black',
-        class: 'text-uppercase mb-4 mb-lg-8',
-        variant: 'outlined'
-      }
-    },
-    title: 'Экологические акции',
-    subtitle: 'Команда АО “ССГПО” активно принимает участие в мероприятиях, направленных на помощь экологии',
-    textBlockAttrs: {
-    },
-    subTitleAttrs: {
-      class: 'text-black text-h5',
-      style: 'max-width: 575px;'
-    },
-    titleAttrs: {
-      class: 'text-black font-weight-semi-bold mb-3 text-36 w-75'
-    },
-    img: {
-      src: 'https://images.unsplash.com/photo-1562043236-559c3b65a6e2?auto=format&fit=crop&q=80&w=2940&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      attrs: {
-        'max-height': 400,
-        'max-width': '100%',
-        class: 'rounded-xl'
-      }
-    },
-    active: false,
-  },
-  {
-    id: 1,
-    chip: {
-      text: 'Социальные проекты',
-      attrs: {
-        color: 'black',
-        class: 'text-uppercase mb-8',
-        variant: 'outlined'
-      }
-    },
-    title: 'Экологические акции',
-    subtitle: 'Команда АО “ССГПО” активно принимает участие в мероприятиях, направленных на помощь экологии',
-    textBlockAttrs: {
-    },
-    subTitleAttrs: {
-      class: 'text-black text-h5',
-      style: 'max-width: 575px;'
-    },
-    titleAttrs: {
-      class: 'text-black font-weight-semi-bold mb-3 text-36 w-75'
-    },
-    img: {
-      src: 'https://images.unsplash.com/photo-1562043236-559c3b65a6e2?auto=format&fit=crop&q=80&w=2940&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      attrs: {
-        'max-height': 400,
-        'max-width': '98%',
-        class: 'rounded-xl'
-      }
-    },
-    active: false,
-  },
-]
-
 </script>
 
 <style lang="scss">
@@ -164,5 +99,11 @@ const slidesSocial = [
 .unused-selector-app {
   color: orange;
 }
+.custom-checkbox .v-input--selection-controls__input {
+  align-items: flex-start;
+}
 
+.custom-checkbox .v-input--checkbox .v-icon {
+  margin-top: 0px; /* Настройте это значение в зависимости от вашего дизайна */
+}
 </style>
