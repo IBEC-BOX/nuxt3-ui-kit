16f2d69d1a30d589ba26722fc0852ea6b8f792ed
Евгений Головкин <evgeniq.golovkin@gmail.com>
2024-04-19 14:48:44 +0600

    update pagination

--- src/runtime/components/Parts/Pagination.vue
+++ src/runtime/components/Parts/Pagination.vue
@@ -1,39 +1,44 @@
 <template>
-  <v-pagination
-    v-model="currentPage"
-    :length="paginationLastPage"
-    :total-visible="5"
-    class="custom-pagination"
-    :class="sidesButton ? 'sides-button' : ''"
-    v-bind="paginationAttrs"
-  >
-    <template #prev>
-      <v-btn
-        variant="text"
-        class="text-none mr-auto px-0"
-        v-bind="paginationButtonAttrs"
-        @click="changePage(-1)"
-      >
-        <v-icon size="20">
-          mdi-arrow-left
-        </v-icon>
-        <span class="ml-2 text-18">{{ textButtonLeftPagination }}</span>
-      </v-btn>
-    </template>
-    <template #next>
-      <v-btn
-        variant="text"
-        class="text-none ml-auto px-0"
-        v-bind="paginationButtonAttrs"
-        @click="changePage(1)"
-      >
-        <span class="mr-2 text-18">{{ textButtonRightPagination }}</span>
-        <v-icon size="20">
-          mdi-arrow-right
-        </v-icon>
-      </v-btn>
-    </template>
-  </v-pagination>
+  <v-sheet class="custom-pagination position-relative bg-transparent">
+    <span class="d-flex d-sm-none mobile-pagination">{{ wordMobilePagination + ' ' + currentPage + ' ' + ellipsisMobilePagination + '  ' + paginationLastPage }}</span>
+
+    <v-pagination
+      v-model="currentPage"
+      :length="paginationLastPage"
+      :total-visible="paginationTotalVisible"
+      :class="sidesButton ? 'sides-button' : ''"
+      v-bind="paginationAttrs"
+    >
+      <template #prev>
+        <v-btn
+          variant="text"
+          class="text-none mr-auto px-0"
+          v-bind="paginationButtonAttrs"
+          @click="changePage(-1)"
+        >
+          <v-icon size="20">
+            {{ iconButtonLeftPagination }}
+          </v-icon>
+          <span class="ml-2 text-18 d-none d-sm-inline">{{ textButtonLeftPagination }}</span>
+        </v-btn>
+      </template>
+      <template #next>
+        <v-btn
+          variant="text"
+          class="text-none ml-auto px-0"
+          v-bind="paginationButtonAttrs"
+          @click="changePage(1)"
+        >
+          <span class="mr-2 text-18 d-none d-sm-inline">{{ textButtonRightPagination }}</span>
+          <v-icon size="20">
+            {{ iconButtonRightPagination }}
+          </v-icon>
+        </v-btn>
+      </template>
+    </v-pagination>
+
+  </v-sheet>
+
 </template>
 
 <script setup lang="ts">
@@ -56,9 +61,14 @@ const currentPage = ref(route.query.page ? parseInt(route.query.page) : 1);
 
 const props = defineProps({
   paginationLastPage: { type: Number, default: () => 1 },
+  paginationTotalVisible: { type: Number, default: () => 5 },
   textButtonLeftPagination: { type: String, default: '' },
   textButtonRightPagination: { type: String, default: '' },
-  sidesButton: { type: Boolean, default: true, }
+  iconButtonLeftPagination: { type: String, default: 'mdi-arrow-left' },
+  iconButtonRightPagination: { type: String, default: 'mdi-arrow-right' },
+  sidesButton: { type: Boolean, default: true, },
+  wordMobilePagination: { type: String, default: '' },
+  ellipsisMobilePagination: { type: String, default: '' },
 })
 
 const emit = defineEmits(['update-pagination'])
@@ -73,13 +83,37 @@ function changePage(direction: number) {
 </script>
 
 <style lang="scss">
-.custom-pagination.sides-button {
-  .v-pagination__prev {
-    margin-right: auto;
+.custom-pagination  {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  .sides-button {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    .v-pagination__item {
+      @media(max-width: 600px) {
+        display: none;
+      }
+    }
+    .v-pagination__list {
+      align-items: center;
+    }
+    .v-pagination__prev {
+      margin-right: auto;
+    }
+    .v-pagination__next {
+      margin-left: auto;
+    }
   }
-  .v-pagination__next {
-    margin-left: auto;
+
+  .mobile-pagination {
+    position: absolute;
+    height: 100%;
+    display: flex;
+    align-items: center;
   }
+
 }
 
 </style>
