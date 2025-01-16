8213cafd9f221c34a6e7081cc34ae6983c0f8f5a
Zh3nyaZh3nya <mr.happy.2000@mail.ru>
2025-01-16 15:57:21 +0500

    added cookie banner erg

---
+++ src/runtime/components/Parts/Cookie/Erg.vue
@@ -0,0 +1,50 @@
+<template>
+  <div v-if="isCookieBannerOpen" class="cookie-banner bg-secondary rounded-t-16">
+    <v-container class="pt-0">
+      <header class="d-flex justify-end">
+        <v-btn variant="text" class="px-0" @click="acceptCookies" min-width="20px" min-height="20px" height="20px">
+          <v-icon icon="mdi-close"></v-icon>
+        </v-btn>
+      </header>
+      <div class="mb-0" v-html="text">
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, defineProps } from 'vue';
+
+const props = defineProps({
+  text: String
+})
+
+const isCookieBannerOpen = ref<boolean>(true);
+
+const acceptCookies = () => {
+  isCookieBannerOpen.value = false;
+  localStorage.setItem('cookiesAccepted', 'true');
+};
+
+const checkCookiesAccepted = () => {
+  const accepted = localStorage.getItem('cookiesAccepted');
+  isCookieBannerOpen.value = accepted !== 'true';
+};
+
+onMounted(() => {
+  checkCookiesAccepted()
+})
+</script>
+
+<style scoped lang="scss">
+.cookie-banner {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: #f9f9f9;
+  box-shadow: 0px -2px 10px rgba(0, 0, 0, 0.1);
+  padding: 16px;
+  z-index: 1000;
+}
+</style>
